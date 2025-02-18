7ce055f49532a31b9487e40befaf2b07f990aa54
katiivashchenko <katiivashchenko@gmail.com>
2025-02-18 05:00:02 +0100

    skript.js

---
+++ skript.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const animationEffect = document.querySelector('.animation-effect');
+    animationEffect.classList.add('appear');
+});
