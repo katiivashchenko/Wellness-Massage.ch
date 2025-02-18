354603a3174c8837e6b3d91e8ea791c7e03a3c96
katiivashchenko <katiivashchenko@gmail.com>
2025-02-18 05:05:23 +0100

    skript.js

--- skript.js
+++ skript.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const animationEffect = document.querySelector('.animation-effect');
     animationEffect.classList.add('appear');
+
+    // Интеграция с Google Maps
+    initMap();
 });
+
+function initMap() {
+    const location = { lat: 47.4727, lng: 9.4974 }; // Пример координат для Rorschach, Швейцария
+    const map = new google.maps.Map(document.getElementById('google-map'), {
+        zoom: 14,
+        center: location
+    });
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
